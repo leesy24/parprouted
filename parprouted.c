a7650b7668a021c841298cad5bef608e01517953
Sang-Young Lee <leesy24@gmail.com>
2019-05-24 17:00:48 +0900

    change path of ip to match with openwrt

--- parprouted.c
+++ parprouted.c
@@ -106,7 +106,7 @@ int route_remove(ARPTAB_ENTRY* cur_entry)
     int success = 1;
     
     if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
-	    "/bin/ip route del %s/32 metric 50 dev %s scope link",
+	    "/usr/sbin/ip route del %s/32 metric 50 dev %s scope link",
 	    inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
     {
 	syslog(LOG_INFO, "ip route command too large to fit in buffer!");
@@ -136,7 +136,7 @@ int route_add(ARPTAB_ENTRY* cur_entry)
     int success = 1;
 
     if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
-	    "/bin/ip route add %s/32 metric 50 dev %s scope link",
+	    "/usr/sbin/ip route add %s/32 metric 50 dev %s scope link",
 	    inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
     {
 	syslog(LOG_INFO, "ip route command too large to fit in buffer!");
